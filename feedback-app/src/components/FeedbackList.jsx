406fd46a70f4b1766f7408998b6c2955990069ac
tengku_asyraf <tengku.asyraf@cognitive-edge.com>
2022-02-06 21:19:20 +0800

    Use framer-motion to add ease out animation adding/removing feedback

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,18 +1,23 @@
 import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
+import { motion, AnimatePresence } from "framer-motion";
 
 function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback yet</p>;
   }
+
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem
+        <motion.div
           key={item.id}
-          item={item}
-          handleDelete={handleDelete}
-        />
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+        </motion.div>
       ))}
     </div>
   );
